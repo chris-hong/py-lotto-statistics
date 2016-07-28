12a5cb016b6823ead38008c3d72b0c6d5ee2480f
sayong.hong <sayong.hong@hanwha.com>
2016-07-28 19:57:37 +0900

    20160728

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
 # this is test page for beginning
 
-greeting = 'HI~ Hello~ Greeting!!~'
+# greeting = 'HI~ Hello~ Greeting!!~'
+#
+# print(greeting)
 
-print(greeting)
+from htmldom import htmldom
+
+dom = htmldom.HtmlDom("http://m.naver.com").createDom()
+
+p = dom.find("*")
+
+print(p.html())
+
+
